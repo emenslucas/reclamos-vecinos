2654f614f687becd780555f0589b164931346665
emenslucas <emens.lucasp@gmail.com>
2025-01-09 22:01:37 -0300

    .

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyAfxNslbKLt5mQqLKGIxacAL1pMpBwATyA",
+  authDomain: "gestion-reclamos-vecinos.firebaseapp.com",
+  projectId: "gestion-reclamos-vecinos",
+  storageBucket: "gestion-reclamos-vecinos.firebasestorage.app",
+  messagingSenderId: "991461778099",
+  appId: "1:991461778099:web:b7a09c07875c6265bbc6c6",
 };
 
 export const app = initializeApp(firebaseConfig);
